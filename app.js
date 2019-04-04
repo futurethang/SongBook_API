5c2b6b6f915f2c624c22688f433cc0eec48e84ce
Kevin <kphyde@gmail.com>
2019-04-04 10:22:31 -0700

    add react app and connect to express server

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const pug = require('pug');
 require('dotenv').config()
 
 // db setup
 let dev_db_url = process.env.DB_HOST;
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
+let mongoDB = process.env.DB_HOST || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
@@ -27,20 +26,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/songs', songsRouter);
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const song_controller = require('../controllers/song.controller')
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('songs', { title: 'SongBook' });
+// });
+router.get('/', song_controller.song_list);
+
 router.get('/newSong', function(req, res, next) {
   res.render('addSong', { title: 'Add a Song' });
 });
 router.get('/editSong', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Edit Song' });
 });
 
 module.exports = router;
